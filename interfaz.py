0ba704737e39d741a4d77768617d09a822dce7e7
Liceth Yaneth Rozo Quintana <liceth@pop-os.localdomain>
2020-12-20 19:01:41 -0500

    templates

--- interfaz.py
+++ interfaz.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template      
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+@app.route("/")
+def home():
+    return render_template("home.html")
+    
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+    
 if __name__ == "__main__":
     app.run(debug=True)
